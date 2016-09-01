e7b849bffca4ca9a9a2f3c9051f1a4191406e3f4
David Tinianow <david.tinianow@gmail.com>
2016-09-01 15:34:09 -0600

    Activity spec passing

--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -32,22 +32,33 @@ describe Activity do
     end
   end
 
-  # it "returns a list of recent activities belonging to the current user" do
-  #   VCR.use_cassette("activity_service_get_followings_activity") do
-  #
-  #     activities = Activity.user_activity(@user)
-  #     activity = activities.first
-  #
-  #     expect(activities.count).to eq 10
-  #     expect(activity).to respond_to :type
-  #     expect(activity).to respond_to :created_at
-  #     expect(activity).to respond_to :actor
-  #     expect(activity).to respond_to :repo
-  #     expect(activity).to respond_to :payload
-  #     expect(activity.type).to eq "PushEvent"
-  #     expect(activity.created_at).to eq "2016-09-01T18:45:44Z"
-  #     expect(activity.actor.login).to eq "dtinianow"
-  #     expect(activity.repo.name).to eq "dtinianow/apicurious"
-  #     expect(activity.payload['commits'].first.message).to eq "Add repo name to user show page"
-  #   end
+  it "returns a list of activities of users followed by the current user" do
+    VCR.use_cassette("activity_service_get_followings_activity") do
+
+      activities = Activity.followings_activity(@user)
+      activity = activities.first
+
+      expect(activities.count).to eq 71
+      expect(activity).to respond_to :type
+      expect(activity).to respond_to :created_at
+      expect(activity).to respond_to :actor
+      expect(activity).to respond_to :repo
+      expect(activity).to respond_to :payload
+      expect(activity.type).to eq "PushEvent"
+      expect(activity.created_at).to eq "2016-09-01T20:18:16Z"
+      expect(activity.actor.login).to eq "ryanflach"
+      expect(activity.repo.name).to eq "ryanflach/treddit"
+      expect(activity.payload['commits'].first.message).to eq "Add SimpleCov for test coverage"
+    end
   end
+
+  it "returns a commit message from a payload" do
+    VCR.use_cassette("activity_service_get_user_activity") do
+
+      activity = Activity.user_activity(@user).first
+
+      expect(activity).to respond_to :payload
+      expect(activity.commit_message).to eq "Organization service and model spec"
+    end
+  end
+end
